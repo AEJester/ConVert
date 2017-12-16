0d0344586cda4340cbd70c9bbd8550fd4f5f0bb9
Ryan Monaghan <aftereffectsjester@gmail.com>
2017-12-16 18:39:35 -0500

    Update index.js
    Created the initial algorithm for converting the text to morse code.

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
 var alphabet = ["abcdefghijklmnopqrstuvwxyz0123456789"];
 var morse = [".-", "-...", "-.-.", "-.." ".", "..-.", "--". "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..", "-----", ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----."];
-var final = "";
 exports.textToMorse = function(text) {
-  for (var i = 0; i < text.length; i++) {
-    for (var q = 0; q < alphabet.length; q++) {
-      final += morse[q];
+    var final = "";
+    for (var i = 0; i < text.length; i++) {
+        for (var q = 0; q < alphabet.length; q++) {
+            final += morse[q];
+        }
     }
-  }
 }
 exports.morseToText = function(morse) {
   var final = "";
   var finalCode = [];
   var finalCodeText = "";
   for (var i = 0; i < morse.length; i++) {
-	  for (var q = 0; q < alphabet.length; q++) {
+	for (var q = 0; q < alphabet.length; q++) {
 	    if (morse[i] == "." || morse[i] == "-") {
 	    	finalCodeText += morse[i];
 	    } else if (morse[i] == " ") {
-        finalCode.push(finalCodeText);
-	      finalCodeText = "";
+        	finalCode.push(finalCodeText);
+	      	finalCodeText = "";
 	    }
-	  }
 	}
-	for (var i = 0; i < finalCode.length; i++) {
-	  for (var q = 0; q < morse.length; q++) {
+    }
+    for (var i = 0; i < finalCode.length; i++) {
+        for (var q = 0; q < morse.length; q++) {
 	    if (finalCode[i] == morse[q]) {
-	      final += alphabet[q];
+	    	final += alphabet[q];
 	    }
-	  }
 	}
+    }
 }
 exports.morseAlphabet = morse;
